e5beeb9b21523e68202d28f58fc2569de21406a0
azumi128 <92067074+azumi128@users.noreply.github.com>
2022-07-05 16:59:27 +0900

    0705
    やりました

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css")
-  eleventyConfig.addPassthroughCopy("./src/images")
-  eleventyConfig.addPassthroughCopy("./src/js")
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/js");
   return {
     dir: {
-      input: "src",
-    },
-  }
-}
+      input: "src"
+    }
+  };
+};
