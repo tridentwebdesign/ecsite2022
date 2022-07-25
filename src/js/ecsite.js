36dd58d693cb85db761ad16a21f75a0b7488e3a3
kenta66448284 <92067021+kenta66448284@users.noreply.github.com>
2022-07-25 18:28:55 +0900

    jsの設定

--- src/js/ecsite.js
+++ src/js/ecsite.js
@@ -1,2 +1,11 @@
-let state = document.querySelector(".item__state");
+let state = document.querySelectorAll(".item__state");
 console.log(state)
+for(let i=0;i<state.length;i++){
+    let zaiko = state[i].innerHTML;
+    console.log(zaiko)
+    if(zaiko=="true"){
+        state[i].innerHTML="在庫有"
+    }else{
+        state[i].innerHTML="在庫無"
+    }
+}
