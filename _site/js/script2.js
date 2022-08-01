7029c3a40137704e01b952e613a2ba6d7a4f81e5
Harunixi <92066951+Harunixi@users.noreply.github.com>
2022-08-01 18:10:48 +0900

    haru

--- _site/js/script2.js
+++
@@ -1,8 +0,0 @@
-const image = document.querySelectorAll(".single__item img");
-const imageMain = document.querySelector(".single__image-main img");
-
-for (let i = 0; i < image.length; i++) {
-    image[i].addEventListener("click", () => {
-        imageMain.src = image[i].src;
-    })
-}
