fa5262887f79d050ed23892a391d84c4af091451
Harunixi <92066951+Harunixi@users.noreply.github.com>
2022-08-01 17:37:56 +0900

    haru
    change 2022/8/1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 const product = document.querySelectorAll(".product__card");
+const button = document.querySelector(".button");
+let count = 0;
+product[9].classList.add("product__card-hidden");
+
+button.addEventListener("click", () => {
+    product[9].classList.toggle("product__card-hidden");
+    count++
+    if (count % 2 !== 1) {
+        button.innerText = "More";
+    } else {
+        button.innerText = "Close";
+    }
+});
